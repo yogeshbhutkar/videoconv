fc87554b5a2ff33ca6e50135dd2a467f6aced4ab
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-02-11 15:25:28 +0530

    feat: add file path flags

--- cmd/generate.go
+++ cmd/generate.go
@@ -4,9 +4,12 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+)
+
+var (
+	inputFilePath  string
+	outputFilePath string
 )
 
 var generateCmd = &cobra.Command{
@@ -14,11 +17,17 @@ var generateCmd = &cobra.Command{
 	Short: "Generate a new gif from mov file",
 	Long:  "Generate a new gif from mov file using ffmpeg.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("generate called")
 
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(generateCmd)
+
+	// Add flags for input and output file paths.
+	generateCmd.Flags().StringVarP(&inputFilePath, "input", "i", "", "Input file path")
+	generateCmd.Flags().StringVarP(&outputFilePath, "output", "o", "", "Output file path")
+
+	// Input path should be mandatory.
+	generateCmd.MarkFlagRequired("input")
 }
